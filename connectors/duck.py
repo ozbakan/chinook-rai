9e17f2da7d0851618681ce07ad0d40bc95bfbe86
tolga <tolga@ozbakan.com>
2022-03-03 16:35:19 +0300

    Test commit

--- connectors/duck.py
+++ connectors/duck.py
@@ -81,3 +81,4 @@ class DuckDBConnector:
 if __name__ == '__main__':
     dc = DuckDBConnector('data/chinook/duckdb/chinook.db')
     print(dc.execute('queries/chinook/sql/common/4a.sql'))
+    print('done')
