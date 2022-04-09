08b2a9c29701709042fd5f7c1ad202a6cebcacfb
tolga <tolga@ozbakan.com>
2022-04-09 11:42:00 +0300

    Add new query

--- queries/chinook/sql/snowflake/6a.sql
+++ queries/chinook/sql/snowflake/6a.sql
@@ -3,12 +3,13 @@
 -- Description: Joins relations. This will produce 412 records, one for 
 -- each invoice.
 
-select c.last_name,
+select
+    c.last_name,
     c.first_name,
     c.email,
     i.customer_id,
     i.invoice_id,
-    i.total::float8
-from invoice as i
-    inner join customer as c 
-    on i.customer_id = c.customer_id
+    i.total :: float8
+from
+    invoice as i
+    inner join customer as c on i.customer_id = c.customer_id
--- tests/query_tester.py
+++ tests/query_tester.py
@@ -198,5 +198,10 @@ class RelTester(unittest.TestCase):
         b = self.b.execute(os.path.join(self.b_dir, '5d.sql'))
         self.assertEqual(a, b)
 
+    def test_inner_join(self):
+        a = self.a.execute(os.path.join(self.a_dir, '6a.rel'))
+        b = self.b.execute(os.path.join(self.b_dir, '6a.sql'))
+        self.assertEqual(a, b)
+
 if __name__ == '__main__':
     unittest.main()
