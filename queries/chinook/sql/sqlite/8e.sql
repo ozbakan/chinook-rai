55be45275e261c18e950dd65b6a2536f3fd985de
tolga <tolga@ozbakan.com>
2022-04-17 15:32:19 +0300

    Minor change

--- queries/chinook/sql/sqlite/8e.sql
+++ queries/chinook/sql/sqlite/8e.sql
@@ -2,9 +2,7 @@
 -- Date:          02-11-2022
 -- Description:   Returns tracks that are not in any of the invoices.
 
-select track_id,
-    composer,
-    name
+select track_id
 from track
 where track_id not in (
         select distinct track_id
